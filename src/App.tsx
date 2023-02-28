e317cb7322ff912d7629385ec26834d467aad93d
kim-seonkyu <seonkyu93@gmail.com>
2023-03-01 00:58:12 +0900

    combineReducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 interface Props {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
 }
@@ -9,7 +9,7 @@ interface Props {
 function App({ value, onIncrement, onDecrement }: Props) {
   return (
     <div className="App">
-      Clicked : {value} times
+      {/* Clicked : {value} times */}
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
     </div>
