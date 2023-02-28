b4e93aa1ebb0a278bb5a7751cd6d0e48c2f6ea16
kim-seonkyu <seonkyu93@gmail.com>
2023-03-01 00:45:46 +0900

    redux counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 
-function App() {
+interface Props {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({ value, onIncrement, onDecrement }: Props) {
   return (
     <div className="App">
-      Clicked: times
-      <button>+</button>
-      <button>-</button>
+      Clicked : {value} times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
     </div>
   );
 }
